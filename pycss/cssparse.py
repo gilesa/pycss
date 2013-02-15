23cb8d1f16eafc19c1cd3e7171dfdb022fad6e84
Andrew Giles <andrew.d.giles@gmail.com>
2013-02-15 16:58:23 -0500

    Added auto browser prefixing.

--- pycss/cssparse.py
+++ pycss/cssparse.py
@@ -1,19 +1,32 @@
+from rules import rules
 
 def parse( f ):
     output = []
-    vars = {}
+    cssvars = {}
     
     for line in f:
         if line.startswith( '$' ):
             splitline = line.split( ':' )
             
             if len( splitline ) == 2:
-                vars.update( { splitline[0]: splitline[1].strip() } )
+                cssvars.update( { splitline[0]: splitline[1].strip() } )
         else:
+            # Variables
             if '$' in line:
                 var = line[line.find( '$' ):line.find( ';' )]
-                if var in vars:
-                    line = line.replace( var, vars[var] )
+                if var in cssvars:
+                    line = line.replace( var, cssvars[var] )
+                    
+            # Vendor prefixes
+            if ':' in line:
+                rule = line[:line.find( ':' )].lstrip()
+                if rule in rules:                    
+                    for prefix in rules[rule]:
+                        output.append( line.replace( rule, "".join( [' '] * ( 6 - len( prefix ) ) ) + '-' + prefix + '-' + rule ) )
+        
+                    line = line.replace( rule, "        " + rule )
+            
+            
             output.append( line )
         
     return "".join( output )
