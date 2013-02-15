a93c384678d712a6c5515865eb8ed92e6be16d11
Andrew Giles <andrew.d.giles@gmail.com>
2013-02-15 18:34:50 -0500

    Added input polling.

--- pycss/pycss.py
+++ pycss/pycss.py
@@ -5,6 +5,7 @@ Created on 2013-02-12
 '''
 import argparse
 import os
+from time import sleep
 import cssparse
 
 
@@ -15,7 +16,23 @@ def ispycss( f ):
         return False
 
 
-def main( path ):
+def parsefiles( files, filecache ):
+    for i, f in enumerate( files ):       
+        modtime = os.path.getmtime( f ) 
+        if modtime > filecache[i]:
+            filecache[i] = modtime
+            
+            pycssfile = open( f, 'r' )
+            output = cssparse.parse( pycssfile )        
+            cssfile = open( f.replace( '.pycss', '.css' ), 'w' )
+            cssfile.write( output.lstrip() )
+            cssfile.close()
+            pycssfile.close()
+        
+            print "Successfully converted '{0}'".format( f ) 
+    
+
+def main( path, watch ):
     
     files = []
     
@@ -36,26 +53,23 @@ def main( path ):
         files[:] = [path + f for f in dirfiles]
     else:
         print "Not a valid file or directory."
-                
-    for f in files:
-        pycssfile = open( f, 'r' )
-        output = cssparse.parse( pycssfile )        
-        cssfile = open( f.replace( '.pycss', '.css' ), 'w' )
-        cssfile.write( output.lstrip() )
-        cssfile.close()
-        pycssfile.close()
-        
-    if len( files ) == 1:
-        msg = "Successfully converted 1 file!"
-    else:
-        msg = "Successfully converted {0} files!".format( len( files ) )
-    print msg
+         
+    filecache = [0] * len( files )
     
+    if watch:
+        while True:
+            parsefiles( files, filecache )
+            sleep( 5 )
+    else:
+        parsefiles( files, filecache )
+             
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser( description='Convert pycss files to valid CSS files.' )
     parser.add_argument( 'input', nargs='?', default='.',
                         help='file to convert or directory in which to convert all pycss files (default: the current directory)' )
+    parser.add_argument( '-w', '--watch', action='store_true',
+                        help='if included, the input is continuously polled for changes and updated' )
     args = parser.parse_args()
     
-    main( args.input )
+    main( args.input, args.watch )
